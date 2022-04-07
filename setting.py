3b53083e4cab30229159b05491e01a76ed86959c
WataruTakahagi <watarut@eqchem.s.u-tokyo.ac.jp>
2022-04-07 13:51:41 +0900

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='yamalab',
+    version='0.0.1',
+    author='Wataru Takahagi, Fumitoshi Matoba',
+    author_email='watarut@g.ecc.u-tokyo.ac.jp',
+    url='https://github.com/WataruTakahagi/YamaLab/',
+    py_modules=['yamalab'],
+    description='Keithley 2401',
+    keywords='Keithley 2401, pymeasure, electrochemistry',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
